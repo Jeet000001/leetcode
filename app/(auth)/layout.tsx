69c5dc414c8372990b7dfc09a28f77a282d44764
Jeet000001 <jeetpaul2222@gmail.com>
2026-07-18 20:03:38 +0530

    Add basic signup and signin feature in the application -use can signup and signin

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+
+
+const layout = ({ children }) => {
+  return (
+    <main className="flex flex-col items-center justify-center h-screen">
+      {children}
+    </main>
+  );
+};
+
+export default layout;
