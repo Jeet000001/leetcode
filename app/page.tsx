7aace252a2478780900228bd854b9aaf19024088
Jeet000001 <jeetpaul2222@gmail.com>
2026-07-18 21:23:14 +0530

    implement user onBoarding - when we signin with clerk the user data will store in postgres

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
+import { onBoardUser } from '@/modules/auth/actions'
 import { UserButton } from '@clerk/nextjs'
-import React from 'react'
 
-const page = () => {
+const page = async() => {
+  await onBoardUser()
   return (
     <div>
       <UserButton />
--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,19 @@
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "./generated/prisma/client";
 
 const globalForPrisma = globalThis as {
   prisma?: PrismaClient;
 };
 
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
 export const db =
-  globalForPrisma.prisma ?? new PrismaClient();
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = db;
